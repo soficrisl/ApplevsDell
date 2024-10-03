c705fae02210bbc9235ad452f88799f6cacc9158
santiago-fernandezTor <santiago.fernandez@correo.unimet.edu.ve>
2024-10-02 23:03:32 -0400

    Creacion de ensamblador

--- src/Clases/Ensamblador.java
+++ src/Clases/Ensamblador.java
@@ -10,23 +10,91 @@ package Clases;
  *
  * @author Katiuska Torres
  */
-public class Ensamblador {
-    private Almacen Almacen;
-    private String Empresa;
-    private int sueldo_por_hora;
-    private int horas_de_trabajo;
+public class Ensamblador extends Thread{
+    private Almacen almacen;
+    private Empresa business;
+    private int days_mls; 
+    private int contadorCompuStandard=0;
      
-    public Ensamblador( int sueldo_por_hora, int horas_de_trabajo,String Empresa,Almacen Almacen) {
-        this.sueldo_por_hora=sueldo_por_hora;
-        this.horas_de_trabajo=horas_de_trabajo;
+   public Ensamblador(Almacen almacen, Empresa business) {
+        this.almacen = almacen;
+        this.business = business;
+        this.days_mls = business.getDays_in_mls();
     }
-    
-    public void Trabajar() {
-        System.out.println("Trabajo");
-    }
-  
-    public void cobrarSueldo() {
-        System.out.println("Trabajo");
+   @Override
+   public void run(){
+   work();}
+    public void work() {
+        while (true) {
+            try {
+                
+
+                if (business.getNombre().equals("Apple")) {
+                    
+                    if (almacen.getPlaca_base() >= 2 && almacen.getCpus() >= 1 && almacen.getMemoria_ram() >= 4 && almacen.getFuente_alimentacion() >= 4) {
+                            Thread.sleep(2*days_mls);
+                            almacen.getSemaforo().acquire(); 
+                            if (contadorCompuStandard >= 5 && almacen.getTarjetas_graficas() >=2 ) {
+                                
+                                almacen.setPlaca_base(almacen.getPlaca_base() - 2);
+                                almacen.setCpus(almacen.getCpus() - 1);
+                                almacen.setMemoria_ram(almacen.getMemoria_ram() - 4);
+                                almacen.setFuente_alimentacion(almacen.getFuente_alimentacion() - 4);
+                                almacen.setTarjetas_graficas(almacen.getTarjetas_graficas() - 2);
+                                almacen.setCompuTarjeta(almacen.getCompuTarjeta() + 1);
+                                contadorCompuStandard = 0;
+                                almacen.getSemaforo().release(); 
+                                Thread.sleep(2*days_mls);
+                                almacen.getSemaforo().acquire();
+                            }
+                            almacen.setPlaca_base(almacen.getPlaca_base() - 2);
+                            almacen.setCpus(almacen.getCpus() - 1);
+                            almacen.setMemoria_ram(almacen.getMemoria_ram() - 4);
+                            almacen.setFuente_alimentacion(almacen.getFuente_alimentacion() - 4);
+                            almacen.setCompuStandard(almacen.getCompuStandard() + 1);
+                            contadorCompuStandard++;
+                            
+                    }
+                    
+                } else if (business.getNombre().equals("Dell")) {
+                    
+                    if (almacen.getPlaca_base() >= 1 && almacen.getCpus() >= 5 && almacen.getMemoria_ram() >= 6 && almacen.getFuente_alimentacion() >= 5 ) {
+                        Thread.sleep(2*days_mls);
+                        
+                        almacen.getSemaforo().acquire(); 
+                        if (contadorCompuStandard >= 3 && almacen.getTarjetas_graficas() >=1) {
+                                
+                                almacen.setPlaca_base(almacen.getPlaca_base() - 1);
+                                almacen.setCpus(almacen.getCpus() - 5);
+                                almacen.setMemoria_ram(almacen.getMemoria_ram() - 6);
+                                almacen.setFuente_alimentacion(almacen.getFuente_alimentacion() - 5);
+                                almacen.setTarjetas_graficas(almacen.getTarjetas_graficas() - 1);
+                                almacen.setCompuTarjeta(almacen.getCompuTarjeta() + 1);
+                                contadorCompuStandard = 0;
+                                almacen.getSemaforo().release(); 
+                                Thread.sleep(2*days_mls);
+                                almacen.getSemaforo().acquire();
+                    }
+                        almacen.setPlaca_base(almacen.getPlaca_base() - 1);
+                        almacen.setCpus(almacen.getCpus() - 5);
+                        almacen.setMemoria_ram(almacen.getMemoria_ram() - 6);
+                        almacen.setFuente_alimentacion(almacen.getFuente_alimentacion() - 5);
+                        almacen.setCompuStandard(almacen.getCompuStandard() + 1);
+                        contadorCompuStandard++;
+                        
+                    
+                    }
+                    
+                }
+
+                almacen.getSemaforo().release(); 
+
+                
+                
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
 
